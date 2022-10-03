cce65faefa23dcfb3906af224c876b6f6412330c
girma <g.world.2006@gmail.com>
2022-10-02 21:39:30 -0400

    percentage aggrigates added

--- SQL Ad-Hoc Codes/Customer_Count.sql
+++ SQL Ad-Hoc Codes/Customer_Count.sql
@@ -1,11 +1,17 @@
 /*********
 	Summary Count of Customers by Country and Gender
 **********/
+With tbl_Table1 As (
+	Select g.EnglishCountryRegionName as Country
+		   , sum(case when c.Gender = 'M' then 1 else 0 end) as MaleCustomers 
+		   , sum(case when c.Gender = 'F' then 1 else 0 end) as FemaleCustomers
+		   , count(c.CustomerKey) as TotalCustomer
+	From [dbo].[DimGeography] as g join DimCustomer as c on c.GeographyKey = g.GeographyKey
+	Group By g.EnglishCountryRegionName
+	
+)
 
-Select g.EnglishCountryRegionName as Country
-	   , sum(case when c.Gender = 'M' then 1 else 0 end) as MaleCustomers 
-	   , sum(case when c.Gender = 'F' then 1 else 0 end) as FemaleCustomers
-	   , count(c.CustomerKey) as TotalCustomer
-From [dbo].[DimGeography] as g join DimCustomer as c on c.GeographyKey = g.GeographyKey
-Group By g.EnglishCountryRegionName
+Select *, (cast(MaleCustomers as decimal(6, 2)) * 100)/TotalCustomer as Pct_Male
+		, (Cast(FemaleCustomers as decimal(6, 2)) * 100)/TotalCustomer as Pct_Female
+From tbl_Table1
 order by Country
