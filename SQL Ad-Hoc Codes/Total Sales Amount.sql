9bb0aa2c0ce9319c6c3e284856f58f2a5132a568
june <g.world.2006@gmail.com>
2023-02-18 16:13:19 -0500

    added grand total

--- SQL Ad-Hoc Codes/Total Sales Amount.sql
+++ SQL Ad-Hoc Codes/Total Sales Amount.sql
@@ -4,7 +4,8 @@ Go
 /*********
 	Count Number of customers and calculate the total sales amount, present the result by Country
 **********/
-;with tbl_Table1
+
+;With tbl_Table1
 As
 (
 	Select g.EnglishCountryRegionName as Country
@@ -12,11 +13,18 @@ As
 		   , Format(sum(case when c.Gender = 'M' then i.SalesAmount else 0 end), 'C2') as MaleTotalSale 
 		   , Format(sum(case when c.Gender = 'F' then 1 else 0 end), 'N0') as FemaleCustomers
 		   , Format(sum(case when c.Gender = 'F' then i.SalesAmount else 0 end), 'C2') as FemaleTotalSale
+		   , Format((sum(case when c.Gender = 'M' then i.SalesAmount else 0 end) + sum(case when c.Gender = 'F' then i.SalesAmount else 0 end)), 'C2') as GrandTotal
 	From [dbo].[DimGeography] as g join DimCustomer as c on c.GeographyKey = g.GeographyKey
 	Join FactInternetSales as i on i.CustomerKey = c.CustomerKey
 	Group By g.EnglishCountryRegionName
 	
 )
 
-Select * from tbl_Table1
-order by Country
+Select	  Country
+		, MaleCustomers
+		, MaleTotalSale
+		, FemaleCustomers
+		, FemaleTotalSale
+		, GrandTotal
+From tbl_Table1
+Order by Country
